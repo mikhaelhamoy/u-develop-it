4ad3d16118f9c3c51d2b60faca849e2513cb2aa8
gagz092-mb <mikhaelhamoy@gmail.com>
2021-09-11 18:48:57 -0400

    get votes result

--- routes/apiRoutes/partyRoutes.js
+++ routes/apiRoutes/partyRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../../db/connection');
 
 // get all parties
-router.get('/api/parties', (req, res) => {
+router.get('/parties', (req, res) => {
   const sql = `SELECT * FROM parties`;
   db.query(sql, (err, rows) => {
       if (err) {
@@ -18,7 +18,7 @@ router.get('/api/parties', (req, res) => {
 });
 
 // get single party
-router.get('/api/party/:id', (req, res) => {
+router.get('/party/:id', (req, res) => {
   const sql = `SELECT * FROM parties WHERE id = ?`;
   const params = [req.params.id];
   db.query(sql, params, (err, row) => {
@@ -34,7 +34,7 @@ router.get('/api/party/:id', (req, res) => {
 });
 
 // delete a party
-router.delete('/api/party/:id', (req, res) => {
+router.delete('/party/:id', (req, res) => {
   const sql = `DELETE FROM parties WHERE id = ?`;
   const params = [req.params.id];
   db.query(sql, params, (err, result) => {
